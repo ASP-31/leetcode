c01c5dcda2e0623313acc1884505e0100a9c1e3a
Arjun S Pai <asp31.2006@gmail.com>
2026-09-22 16:32:36 +0530

    Add: 51. N-Queens (python3)

---
+++ solutions/uncategorized/0051-n-queens/README.md
@@ -0,0 +1,9 @@
+# 51. N-Queens
+
+**Difficulty:** Hard
+**Language:** python3
+**URL:** [LeetCode](https://leetcode.com/problems/n-queens/)
+**Confidence:** ★★★☆☆ (3/5)
+
+---
+*Solved on 9/22/2026*
---
+++ solutions/uncategorized/0051-n-queens/solution.py
@@ -0,0 +1,8 @@
+# 51. N-Queens
+# Problem: https://leetcode.com/problems/n-queens/
+# Difficulty: Hard
+# Language: python3
+
+class Solution:
+    def solveNQueens(self, n: int) -> list[list[str]]:
+        
