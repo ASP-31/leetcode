bf581c3228236464490a3395006ab20f49efde79
Arjun S Pai <asp31.2006@gmail.com>
2026-09-24 20:08:02 +0530

    Update: Move Zeroes (python3)

--- solutions/uncategorized/0000-is-subsequence/README.md
+++ solutions/uncategorized/0000-is-subsequence/README.md
@@ -1,9 +1,14 @@
 # Move Zeroes
 
 **Difficulty:** Easy
-**Language:** python
+**Language:** python3
 **URL:** [LeetCode](https://leetcode.com/problems/is-subsequence/)
 **Confidence:** ★★★☆☆ (3/5)
+
+## Performance
+
+- **Runtime:** 0 ms
+- **Memory:** 0 MB
 
 ---
 *Solved on 9/24/2026*
--- solutions/uncategorized/0000-is-subsequence/solution.py
+++ solutions/uncategorized/0000-is-subsequence/solution.py
@@ -1,20 +1,13 @@
 # Move Zeroes
 # Problem: https://leetcode.com/problems/is-subsequence/
 # Difficulty: Easy
-# Language: python
+# Language: python3
 
-class Solution(object):
-    def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        count=0
-        for i in nums:
-            if i != 0:
-                nums[count]=i
-                count+=1
-            
-        while count < len(nums):
-            nums[count]=0
-            count+=1
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i,j=0,0
+        while i < len(s) and j< len(t):
+            if s[i]==t[j]:
+                i+=1
+            j+=1
+        return i == len(s)
